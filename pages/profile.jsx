37a098a8f22ce1d4dd1adc07b457fe066ad5ea79
quangthuan952 <thuan.hoangquang952@gmail.com>
2023-08-02 08:01:25 +0700

    fix login

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useRef} from 'react'
+import React, {useEffect, useState} from 'react'
 import 'react-toastify/dist/ReactToastify.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import {faPencil} from '@fortawesome/free-solid-svg-icons'
@@ -6,7 +6,7 @@ import FieldInfo from '../components/FieldInfo'
 import {UserApi} from '../apis/user'
 import eventEmitter from '../utils/eventEmitter'
 import {signIn} from 'next-auth/react'
-import {Button, Form, Modal, OverlayTrigger, Popover} from "react-bootstrap";
+import {Button, Modal} from "react-bootstrap";
 
 const menu = [
     {
@@ -24,7 +24,7 @@ async function handlerSignGoogle() {
 const PersonalDetail = () => {
     const [info, setInfo] = useState({})
     const [showPopover, setShowPopover] = useState(false)
-    const target = useRef(null);
+    const [data, setData] = useState(null)
     const [showEdit, setShowEdit] = useState(false)
     const [accounts, setAccounts] = useState([])
     useEffect(() => {
@@ -48,7 +48,12 @@ const PersonalDetail = () => {
             })
         })
     }, [])
-    const ShowDelete = (props) => {
+    const handleOkModalCheck = () => {
+        const cc = accounts.filter(va => va._id !== data._id)
+        setAccounts(cc)
+        setShowPopover(!showPopover)
+    }
+    const ShowCheck = (props) => {
         if (!showPopover) {
             return null
         }
@@ -62,78 +67,85 @@ const PersonalDetail = () => {
                 centered
             >
                 <Modal.Body>
-                  <div>Bạn có chắc chắn muốn xóa</div>
+                    <div>Bạn có chắc chắn muốn xóa</div>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant={'light'} onClick={props.onHide}>Hủy</Button>
-                    {/*<Button variant={'light'} onClick={handleOkModalDelete}>Ok</Button>*/}
+                    <Button variant={'light'} onClick={handleOkModalCheck}>Ok</Button>
                 </Modal.Footer>
             </Modal>
         )
     }
     return (
         <>
-        <div className={'bg-gray-200 min-h-screen'}>
-            <div className={'max-w-[1200px] mx-auto lg:grid grid-cols-3 gap-x-6 lg:py-8 p-2'}>
-                <div className={'col-span-1 bg-white rounded-lg py-4 px-6 mt-4'}>
-                    <div className={'py-2'}>
-                        <div className={'flex gap-x-3 items-center'}>
-                            <div className={''}>
-                                <img src={info?.avatar} alt={''} className={'rounded-full object-top'}/>
+            <div className={'bg-gray-200 min-h-screen'}>
+                <div className={'max-w-[1200px] mx-auto lg:grid grid-cols-3 gap-x-6 lg:py-8 p-2'}>
+                    <div className={'col-span-1 bg-white rounded-lg py-4 px-6 mt-4'}>
+                        <div className={'py-2'}>
+                            <div className={'flex gap-x-3 items-center'}>
+                                <div className={''}>
+                                    <img src={info?.avatar} alt={''} className={'rounded-full object-top'}/>
+                                </div>
+                                <div
+                                    className={'flex flex-col gap-y-3 justify-center w-fit items-center relative cursor-pointer font-bold'}>
+                                    {info?.name}
+                                </div>
                             </div>
-                            <div
-                                className={'flex flex-col gap-y-3 justify-center w-fit items-center relative cursor-pointer font-bold'}>
-                                {info?.name}
-                            </div>
+                            <div className="divider horizontal !my-2"></div>
+                            <ul className={'flex flex-col py-2 !ml-0 pl-0'}>
+                                {menu.map((item, index) =>
+                                    <li key={item.id}
+                                        className={`p-2 cursor-pointer mb-2 ${index === 0 ? 'bg-violet-400 text-white font-bold rounded-md ' : ''}`}>{item.title}</li>
+                                )}
+                            </ul>
                         </div>
-                        <div className="divider horizontal !my-2"></div>
-                        <ul className={'flex flex-col py-2 !ml-0 pl-0'}>
-                            {menu.map((item, index) =>
-                                <li key={item.id}
-                                    className={`p-2 cursor-pointer mb-2 ${index === 0 ? 'bg-violet-400 text-white font-bold rounded-md ' : ''}`}>{item.title}</li>
-                            )}
-                        </ul>
                     </div>
-                </div>
-                <div className={'col-span-2 bg-white rounded-lg py-4 px-6 mt-4 lg:mt-0'}>
-                    <div className={'flex gap-x-5'}>
-                        <div className={'w-full'}>
-                            <FieldInfo value={info?.name} icon={faPencil} label={'Tên hiển thị'} isShowEdit={showEdit}/>
-                            <FieldInfo label={'Tài khoản:'}/>
-                            {(accounts || []).map((account) => (
-                                <div className={'flex gap-x-3 items-center border border-gray-100 rounded-xl mb-2'}>
-                                    <div className={'w-full flex justify-between'}>
-                                        <div className={'flex'}>
-                                            <div className={'p-2'}>
-                                                <img src={account?.photo} alt={''}
-                                                     className={'rounded-circle object-top w-10 h-10'}/>
-                                            </div>
-                                            <div
-                                                className={'flex flex-col gap-y-3 justify-center w-fit items-center relative cursor-pointer font-bold'}>
-                                                {account?.email}
+                    <div className={'col-span-2 bg-white rounded-lg py-4 px-6 mt-4 lg:mt-0'}>
+                        <div className={'flex gap-x-5'}>
+                            <div className={'w-full'}>
+                                <FieldInfo value={info?.name} icon={faPencil} label={'Tên hiển thị'}
+                                           isShowEdit={showEdit}/>
+                                <FieldInfo label={'Tài khoản:'}/>
+                                {(accounts || []).map((account) => (
+                                    <div className={'flex gap-x-3 items-center border border-gray-100 rounded-xl mb-2'}>
+                                        <div className={'w-full flex justify-between'}>
+                                            <div className={'flex'}>
+                                                <div className={'p-2'}>
+                                                    <img src={account?.photo} alt={''}
+                                                         className={'rounded-circle object-top w-10 h-10'}/>
+                                                </div>
+                                                <div
+                                                    className={'flex flex-col gap-y-3 justify-center w-fit items-center relative cursor-pointer font-bold'}>
+                                                    {account?.email}
+                                                </div>
                                             </div>
                                         </div>
+                                        {
+                                           accounts.length !== 1 &&  <div className={'cursor-pointer mr-3'} onClick={() => {
+                                                console.log(account)
+                                                setData(account)
+                                                setShowPopover(true)
+                                            }}>
+                                                <i className="bi bi-x-circle-fill"></i>
+                                            </div>
+                                        }
                                     </div>
-                                    <div className={'cursor-pointer mr-3'} onClick={() => setShowPopover(true)}>
-                                            <i className="bi bi-x-circle-fill"></i>
-                                      </div>
-                                </div>
-                            ))}
-                            {(accounts || []).length < 5 &&
+                                ))}
+                                {(accounts || []).length < 5 &&
                                 <button
                                     className={'w-full flex justify-center items-center h-14 rounded-xl border border-gray-100'}><span
                                     className={'text-3xl'} onClick={async () => {
                                     await localStorage.setItem('account', true)
                                     handlerSignGoogle()
                                 }}>+</span></button>}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    <ShowDelete
-        show={showPopover}
-        onHide={() => setShowPopover(false)}/>
+            <ShowCheck
+                show={showPopover}
+                onHide={() => setShowPopover(false)}/>
         </>
     )
 }
